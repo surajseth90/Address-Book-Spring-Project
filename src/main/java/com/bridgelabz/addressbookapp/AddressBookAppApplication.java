15725b4aedde49a7a1b865959584a6a3dbb961ca
surajseth90 <surajseth90@gmail.com>
2021-06-25 20:02:20 +0530

    Added logging levels

--- src/main/java/com/bridgelabz/addressbookapp/AddressBookAppApplication.java
+++ src/main/java/com/bridgelabz/addressbookapp/AddressBookAppApplication.java
@@ -2,13 +2,17 @@ package com.bridgelabz.addressbookapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @SpringBootApplication
 public class AddressBookAppApplication {
 
 	public static void main(String[] args) {
-
-		SpringApplication.run(AddressBookAppApplication.class, args);
+		ApplicationContext context = SpringApplication.run(AddressBookAppApplication.class, args);
+		log.info("Address Book app started in {} Enviornment ",context.getEnvironment().getProperty("enviornment"));
 	}
 
 }
